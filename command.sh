0eca5e25f9b6f8cb6bfe2a9c800874c7ae544bbd
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-08-08 10:58:18 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp target/sbibank.war /home/shankar/Desktop/Git/sbibank/cookbooks/ola_war_deploy/files
 knife ssl check
-knife cookbook upload ola_war_deploy
+knife cookbook upload /home/shankar/Desktop/Git/sbibank/cookbooks/ola_war_deploy
 docker build -t="jaisriram111/sbi-img" .
 docker run -it -d --name sbi jaisriram111/sbi-img /bin/bash
 docker start sbi
