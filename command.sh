89ba8be94c435613baa7db7f9769a978479e4ac4
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-10-16 08:03:14 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp target/sbibank.war /home/shankar/Desktop/Git/sbibank/cookbooks/nginx/files
-docker build -t="jaisriram111/sbi-img" .
+#docker build -t="jaisriram111/sbi-img" .
 docker run -it -d --name sbi jaisriram111/sbi-img /bin/bash
 docker start sbi
 
